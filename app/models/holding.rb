5dcb5be04ff1b0a209f547e7ea1877be60cc4044
Alexandre Coppens <alexcppns@gmail.com>
2018-06-25 17:13:40 -0400

    fix(models): add missing associations in models

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -1,3 +1,3 @@
 class Holding < Sequel::Model
-
+  many_to_one :holding_type, key: :code, primary_key: :code
 end
--- app/models/holding_type.rb
+++ app/models/holding_type.rb
@@ -1,3 +1,3 @@
 class HoldingType < Sequel::Model
-
+  one_to_many :holdings, primary_key: :code, key: :code
 end
