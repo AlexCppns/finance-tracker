d7b53c95584642dcbda7c86371bca175ef4ae38b
Alexandre Coppens <alexcppns@gmail.com>
2018-06-25 20:26:05 -0400

    chore(seeds): update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+HoldingType.dataset.delete
 
 HoldingType.create(code: 'retirement', name: 'Retirement funds')
 HoldingType.create(code: 'stock_market', name: 'Stock market')
 HoldingType.create(code: 'crypto_market', name: 'Crypto market')
+
+
+Holding.dataset.delete
+
+Holding.create(code: 'crypto_market', value: 1.2, currency: "USD", created_at: (3.months).ago)
+Holding.create(code: 'crypto_market', value: 3.2, currency: "USD", created_at: (2.months).ago)
+Holding.create(code: 'crypto_market', value: 3.5, currency: "USD", created_at: (2.months + 1.week).ago)
+Holding.create(code: 'crypto_market', value: 3.8, currency: "USD", created_at: (1.months).ago)
+
+Holding.create(code: 'retirement', value: 2.2, currency: "USD", created_at: (3.months).ago)
+Holding.create(code: 'retirement', value: 2.8, currency: "USD", created_at: (2.months).ago)
+Holding.create(code: 'retirement', value: 2.9, currency: "USD", created_at: (1.months).ago)
+
+Holding.create(code: 'stock_market', value: 6.2, currency: "USD", created_at: (3.months).ago)
+Holding.create(code: 'stock_market', value: 7.8, currency: "USD", created_at: (2.months).ago)
+Holding.create(code: 'stock_market', value: 9.4, currency: "USD", created_at: (1.months).ago)
