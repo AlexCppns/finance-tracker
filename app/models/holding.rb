5eacc7a8580d20308fbcbdfc988b5de19d6cd8d4
Alexandre Coppens <alexcppns@gmail.com>
2018-06-25 20:02:52 -0400

    feature(holdings): database query for monthly data

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -1,3 +1,12 @@
 class Holding < Sequel::Model
   many_to_one :holding_type, key: :code, primary_key: :code
+
+  def self.monthly_dataset
+    select(:value, :created_at, :code).filter(id: monthly_ids_dataset).order(Sequel.asc(:created_at))
+  end
+
+  def self.monthly_ids_dataset
+    select(Sequel.lit("max(id) as id"))
+        .group(Sequel.lit("date_trunc('month', created_at)"), :code)
+  end
 end
